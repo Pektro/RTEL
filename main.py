dd3df9f7fa9947f6e5829e35d1311183e7bcf21d
Pektro <pedrorafael.drt@gmail.com>
2024-09-26 11:17:25 +0100

    final version

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# B. Work 
- 
-# Implement a program in C that generates intervals between the arrival of 
+# Implement a program in that generates intervals between the arrival of 
 # consecutive events: 
  
 #  - inputs:  
@@ -12,6 +10,7 @@
 
 import math
 import random
+from matplotlib import pyplot
 
 class Event:
     def __init__(self, event_type, time):
@@ -20,7 +19,7 @@ class Event:
 
 class Simulation:
 
-    def __init__(self, lambda_=1, miu_=3, samples_nr=1000, T=1000, max_resources=1000):
+    def __init__(self, lambda_=10, miu_=0.5, samples_nr=1000, T=1000, max_resources=500):
         self.lambda_ = lambda_
         self.miu_ = miu_
         self.samples_nr = samples_nr
@@ -32,12 +31,15 @@ class Simulation:
         self.rejected_calls = 0
 
         self.histogram = {}
+        self.time_plot = {}
+        self.estimator = 0
         self.estimator = 0
         self.time = 0
 
     def call_arrival(self):
 
         c = - math.log(random.uniform(0,1)) / self.lambda_
+        c = round(c, 3)
         if c in self.histogram.keys():
             self.histogram[c] += 1
         else:
@@ -45,41 +47,53 @@ class Simulation:
 
         next_arrival = self.time + c
         self.events.append(Event("arrival", next_arrival))
+        
+        #sort events by time
+        self.events.sort(key=lambda event: event.time)
+
+        self.time_plot[next_arrival] = 1
 
         if self.active_calls < self.max_resources:
             self.active_calls += 1
             next_departure = self.time - math.log(random.uniform(0,1)) / self.miu_
             self.events.append(Event("departure", next_departure))
+            self.events.sort(key=lambda x: x.time)
+            self.time_plot[next_departure] = 2
         else:
             self.rejected_calls += 1
-
-        print("Call Arrival")
 
     def call_departure(self):
 
         if self.active_calls > 0:
             self.active_calls -= 1
-            print("Call Departure")
 
     def run(self):
 
-        # Generate random number of active calls
-        self.active_calls = random.randint(1, self.samples_nr/5)
+        # Simulation parameters
+        print(f'Simulation parameters: lambda={self.lambda_}, miu={self.miu_}, samples_nr={self.samples_nr}, T={self.T}, max_resources={self.max_resources}\n')
+        print(">> Simulation started")
 
-        for i in range(self.active_calls):
-            time = 0
-            next_departure = time - math.log(random.uniform(0,1)) / self.miu_
+        # Generate random number of active calls
+        self.active_calls = random.randint(1, int(self.samples_nr/5))
+        print(f'System started with: {self.active_calls} active calls\n')
+        i = self.active_calls
+        time = 0
+
+        for j in range(i):
+            s = - math.log(random.uniform(0,1)) / self.miu_
+            time += s
+            next_departure = time
             self.events.append(Event("departure", next_departure))
 
         # Generate first arrival
         next_arrival = self.time - math.log(random.uniform(0,1)) / self.lambda_
-        self.events.append(Event("arrival", next_arrival))        
-
-        print("Simulation started")
+        self.events.append(Event("arrival", next_arrival))  
+        self.events.sort(key=lambda event: event.time)
 
         # Run simulation
-        while self.samples_nr > 0:
+        while self.samples_nr + i > 0:
             event = self.events.pop(0)
+            self.time = event.time
 
             if event.event_type == "arrival":
                 self.call_arrival()
@@ -88,10 +102,20 @@ class Simulation:
 
             self.samples_nr -= 1
 
+        self.estimator = sum([k*v for k,v in self.histogram.items()]) / sum(self.histogram.values())
+        self.estimator = round(self.estimator, 3)
+
+        print(">> Simulation ended")
+        print(f'Average time between the arrival of events: {self.estimator} (expected: {1/self.lambda_})')
+        print("Number of rejected calls: ", self.rejected_calls)
 
 if __name__ == "__main__":
 
-    sim = Simulation(lambda_=5, samples_nr=1000)
+    sim = Simulation(lambda_=5, samples_nr=2000)
     sim.run()
 
-    print(sim.histogram)
+    hist = pyplot.bar(sim.histogram.keys(), sim.histogram.values(), width=0.01)
+    
+    pyplot.show()
+
+    
