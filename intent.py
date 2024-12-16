6b74805192251ac7679f29e9c317a98d8dbb5e28
António Lopes <131278549+antoniojglopes@users.noreply.github.com>
2024-12-16 12:51:28 +0000

    Connect and Disconnect

---
+++ intent.py
@@ -0,0 +1,98 @@
+import requests
+import json
+
+# Define a mapping of host names to MAC addresses
+HOST_MAC_MAPPING = {
+    "h1": "00:00:00:00:00:01",
+    "h2": "00:00:00:00:00:02",
+    "h3": "00:00:00:00:00:03",
+    "h4": "00:00:00:00:00:04",
+    "h5": "00:00:00:00:00:05",
+    "h6": "00:00:00:00:00:06",
+    "h7": "00:00:00:00:00:07",
+    "h8": "00:00:00:00:00:08",
+    "h9": "00:00:00:00:00:09",
+    "h10": "00:00:00:00:00:10",
+    "h11": "00:00:00:00:00:11",
+    "h12": "00:00:00:00:00:12"
+}
+
+def send_intent(intent_type, src_host, dst_host):
+    """
+    Function to send intent to Ryu controller via REST API.
+    :param intent_type: Type of intent ('allow' or 'isolate')
+    :param src_host: Source host (e.g., 'h1')
+    :param dst_host: Destination host (e.g., 'h5')
+    """
+    url = "http://localhost:8080/intent/add"
+    headers = {"Content-Type": "application/json"}
+
+    # Resolve MAC addresses from host names
+    src_mac = HOST_MAC_MAPPING.get(src_host)
+    dst_mac = HOST_MAC_MAPPING.get(dst_host)
+
+    if not src_mac or not dst_mac:
+        print(f"Error: Invalid host name(s). Please check your input ({src_host}, {dst_host}).")
+        return
+
+    payload = {
+        "type": intent_type,
+        "src": src_mac,
+        "dst": dst_mac
+    }
+
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        if response.status_code == 200:
+            print("Success:", response.json())
+        elif response.status_code == 400:
+            print("Invalid Input:", response.json())
+        else:
+            print("Error:", response.status_code, response.json())
+    except Exception as e:
+        print("Failed to send intent:", str(e))
+
+
+def main():
+    print("#########################################")
+    print("##                                     ##")
+    print("##   Welcome to the IBN Demonstrator!  ##")
+    print("##                                     ##")
+    print("#########################################")
+    print("Created by: Antonio Lopes & Pedro Duarte")
+    print("Type 'exit' anytime to quit.\n")
+
+    while True:
+        print("Available Intent Types:")
+        print("1. Allow Communication")
+        print("2. Isolate Hosts")
+        intent_choice = input("Enter choice (1 or 2): ").strip()
+
+        if intent_choice.lower() == 'exit':
+            print("Exiting...")
+            break
+
+        if intent_choice == '1':
+            intent_type = 'allow'
+        elif intent_choice == '2':
+            intent_type = 'isolate'
+        else:
+            print("Invalid choice. Try again.")
+            continue
+
+        src_host = input("Enter Source Host (e.g., h1): ").strip()
+        if src_host.lower() == 'exit':
+            break
+
+        dst_host = input("Enter Destination Host (e.g., h5): ").strip()
+        if dst_host.lower() == 'exit':
+            break
+
+        # Send intent
+        print(f"\nSending '{intent_type}' intent from {src_host} to {dst_host}...")
+        send_intent(intent_type, src_host, dst_host)
+        print("\n" + "-"*50 + "\n")
+
+
+if __name__ == "__main__":
+    main()
--- ryu.py
+++ ryu.py
@@ -1,94 +1,166 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, set_ev_cls
+from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_0
 from ryu.lib.mac import haddr_to_bin
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
+from ryu.lib.packet import packet, ethernet, ether_types
+from ryu.app.wsgi import WSGIApplication, ControllerBase, route
+from webob import Response
+import json
+
+# WSGI Controller Name
+snowflake_instance_name = 'snowflake_api_app'
+
 
 class SnowflakeIntentBasedSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+    _CONTEXTS = {'wsgi': WSGIApplication}
 
     def __init__(self, *args, **kwargs):
         super(SnowflakeIntentBasedSwitch, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        self.datapaths = {}  # Map of dpid -> datapath objects
+        self.intents = {'allow': [], 'isolate': []}
+
+        # Register WSGI controller
+        wsgi = kwargs['wsgi']
+        wsgi.register(SnowflakeAPI, {snowflake_instance_name: self})
 
     def add_flow(self, datapath, match, actions, priority=1):
-        ofproto = datapath.ofproto
+        """Add a flow entry."""
         mod = datapath.ofproto_parser.OFPFlowMod(
-            datapath=datapath, match=match, cookie=0,
-            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+            datapath=datapath, match=match, command=datapath.ofproto.OFPFC_ADD,
             priority=priority, actions=actions)
         datapath.send_msg(mod)
 
-    def add_host_communication_rule(self, datapath, src, dst, out_port):
+    def delete_flow(self, datapath, src, dst):
+        """Delete a flow entry based on src and dst MAC addresses."""
+        parser = datapath.ofproto_parser
+
+        # Match and delete flows in both directions
+        for direction in [(src, dst), (dst, src)]:
+            match = parser.OFPMatch(dl_src=haddr_to_bin(direction[0]), dl_dst=haddr_to_bin(direction[1]))
+            mod = parser.OFPFlowMod(
+                datapath=datapath, match=match,
+                command=datapath.ofproto.OFPFC_DELETE
+            )
+            datapath.send_msg(mod)
+
+    def apply_intent(self, datapath):
+        """Apply all intents (allow and isolate) to the datapath."""
+        for src, dst in self.intents['isolate']:
+            self.add_isolation_rule(datapath, src, dst)
+
+        for src, dst in self.intents['allow']:
+            self.add_host_communication_rule(datapath, src, dst)
+
+    def add_intent(self, intent_type, src=None, dst=None):
+        """Handle dynamic addition of intents."""
+        for dpid, datapath in self.datapaths.items():
+            if intent_type == 'allow':
+                self.logger.info("Allowing communication: %s -> %s", src, dst)
+                # Remove isolation rules for the same pair
+                self.delete_flow(datapath, src, dst)
+                self.delete_flow(datapath, dst, src)
+                # Remove from isolation intents
+                self.intents['isolate'] = [
+                    (s, d) for s, d in self.intents['isolate'] if (s, d) != (src, dst) and (s, d) != (dst, src)
+                ]
+                # Add to allow intents
+                if (src, dst) not in self.intents['allow']:
+                    self.intents['allow'].append((src, dst))
+
+            elif intent_type == 'isolate':
+                self.logger.info("Isolating communication: %s <-> %s", src, dst)
+                # Remove from allow intents
+                self.intents['allow'] = [
+                    (s, d) for s, d in self.intents['allow'] if (s, d) != (src, dst) and (s, d) != (dst, src)
+                ]
+                # Add to isolate intents
+                if (src, dst) not in self.intents['isolate']:
+                    self.intents['isolate'].append((src, dst))
+
+            # Reapply all intents for the current datapath
+            self.apply_intent(datapath)
+
+    def add_host_communication_rule(self, datapath, src, dst):
+        """Allow communication between specific hosts."""
         parser = datapath.ofproto_parser
         match = parser.OFPMatch(dl_src=haddr_to_bin(src), dl_dst=haddr_to_bin(dst))
-        actions = [parser.OFPActionOutput(out_port)]
+        actions = [parser.OFPActionOutput(ofproto_v1_0.OFPP_FLOOD)]
         self.add_flow(datapath, match, actions, priority=2)
 
     def add_isolation_rule(self, datapath, src, dst):
+        """Isolate specific hosts by dropping traffic."""
         parser = datapath.ofproto_parser
         match = parser.OFPMatch(dl_src=haddr_to_bin(src), dl_dst=haddr_to_bin(dst))
-        self.add_flow(datapath, match, [], priority=3)  # No actions = drop traffic
-
-    def add_http_priority_rule(self, datapath, in_port, out_port):
-        parser = datapath.ofproto_parser
-        match = parser.OFPMatch(
-            in_port=in_port,
-            dl_type=ether_types.ETH_TYPE_IP,
-            nw_proto=6,  # TCP protocol
-            tp_dst=80    # HTTP traffic
-        )
-        actions = [parser.OFPActionOutput(out_port)]
-        self.add_flow(datapath, match, actions, priority=4)
+        self.add_flow(datapath, match, [], priority=3)  # Drop rule with high priority
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
+        dpid = datapath.id
         ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            return
+            return  # Ignore LLDP packets
 
         dst = eth.dst
         src = eth.src
-        dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
-
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
-
-        # Learn MAC address to avoid FLOOD next time
         self.mac_to_port[dpid][src] = msg.in_port
 
-        # Define output port
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
+        out_port = self.mac_to_port[dpid].get(dst, ofproto.OFPP_FLOOD)
 
-        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
-
-        # Install flow rules
-        if out_port != ofproto.OFPP_FLOOD:
-            # Example intents for your topology
-            if src == '00:00:00:00:00:01' and dst == '00:00:00:00:00:03':
-                self.add_host_communication_rule(datapath, src, dst, out_port)
-            elif src == '00:00:00:00:00:05' or dst == '00:00:00:00:00:05':
-                self.add_isolation_rule(datapath, src, dst)
-            elif eth.ethertype == ether_types.ETH_TYPE_IP:
-                self.add_http_priority_rule(datapath, msg.in_port, out_port)
-
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-
-        out = datapath.ofproto_parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
-            actions=actions, data=data)
+        # Forward packet out if no intent matches
+        actions = [parser.OFPActionOutput(out_port)]
+        data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
+        out = parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id,
+            in_port=msg.in_port, actions=actions, data=data)
         datapath.send_msg(out)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, MAIN_DISPATCHER)
+    def switch_features_handler(self, ev):
+        """Handle new switch connection."""
+        datapath = ev.msg.datapath
+        self.datapaths[datapath.id] = datapath
+        self.logger.info("Switch connected: %s", datapath.id)
+        self.apply_intent(datapath)  # Reapply intents to the new switch
+
+    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def state_change_handler(self, ev):
+        """Handle switch state changes."""
+        datapath = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            self.datapaths[datapath.id] = datapath
+        elif ev.state == DEAD_DISPATCHER:
+            self.datapaths.pop(datapath.id, None)
+
+
+# WSGI REST API Controller
+class SnowflakeAPI(ControllerBase):
+    def __init__(self, req, link, data, **config):
+        super(SnowflakeAPI, self).__init__(req, link, data, **config)
+        self.snowflake_app = data[snowflake_instance_name]
+
+    @route('add_intent', '/intent/add', methods=['POST'])
+    def add_intent(self, req, **kwargs):
+        try:
+            body = json.loads(req.body)
+            intent_type = body.get('type')
+            src = body.get('src')
+            dst = body.get('dst')
+
+            if intent_type in ['allow', 'isolate'] and src and dst:
+                self.snowflake_app.add_intent(intent_type, src, dst)
+                return Response(status=200, body=json.dumps({'status': 'success'}))
+            else:
+                return Response(status=400, body=json.dumps({'status': 'invalid input'}))
+        except Exception as e:
+            return Response(status=500, body=json.dumps({'error': str(e)}))
